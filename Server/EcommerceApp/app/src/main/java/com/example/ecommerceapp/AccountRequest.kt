b19e68926562707c4187bff54131f99f0c6af2b1
IT21319860 <it21319860@my.sliit.lk>
2024-10-08 17:47:46 +0530

    wrong

--- Server/EcommerceApp/app/src/main/java/com/example/ecommerceapp/AccountRequest.kt
+++
@@ -1,4 +0,0 @@
-package com.example.ecommerceapp
-
-
-data class AccountRequest(val email: String, val password: String)
--- Server/EcommerceApp/app/src/main/java/com/example/ecommerceapp/Feedback.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerceapp
-
-data class Feedback(
-    val Vname: String,
-    val rank: String,
-    val comment: String
-)
